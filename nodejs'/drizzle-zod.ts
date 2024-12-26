52427c758336b37fef6d937168041f3370a6d49e
tigawanna <denniskinuthiawaweru@gmail.com>
2024-12-26 20:36:23 +0300

    Remove unused files and configurations to streamline project structure

--- nodejs'/drizzle-zod.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, text, integer } from "drizzle-orm/pg-core";
-import { createSelectSchema } from "drizzle-zod";
-const users = pgTable("users", {
-  id: integer().generatedAlwaysAsIdentity().primaryKey(),
-  name: text().notNull(),
-  age: integer().notNull(),
-});
-const userSelectSchema = createSelectSchema(users);
