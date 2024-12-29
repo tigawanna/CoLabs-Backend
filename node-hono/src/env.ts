f5a35850936f80bdb6ab01ce605ec0c519843a44
tigawanna <denniskinuthiawaweru@gmail.com>
2024-12-29 10:15:38 +0300

    Add random token generation, enhance environment variables, and update package scripts

--- node-hono/src/env.ts
+++ node-hono/src/env.ts
@@ -5,12 +5,16 @@ export const env = {
   port: parseInt(process.env.PORT || "3000"),
   dbUrl: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
+  BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+  BETTER_AUTH_URL: process.env.BETTER_AUTH_URL
 };
 
 const envScheme = z.object({
   port: z.number(),
   dbUrl: z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
 });
 
 export const envVariables = envScheme.parse(env);
